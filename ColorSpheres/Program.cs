79dde4923290e6c69843c009c813ff21d5b7b27a
DinisB <dinisbarroso.dev@gmail.com>
2025-04-03 21:13:21 +0100

    Colors and Spheres - Exericio 3

---
+++ ColorSpheres/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ColorSpheres
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Color green = new Color(0,255,0);
+            Sphere sphere1 = new Sphere(green, 20);
+            Color red = new Color(255,0,0);
+            Sphere sphere2 = new Sphere(red, 10);
+            Color yellow = new Color(255,255,0);
+            Sphere sphere3 = new Sphere(yellow, 30);
+            sphere1.Throw();
+            sphere2.Pop();
+            sphere3.Pop();
+            sphere3.Throw();
+
+            sphere1.PrintState();
+            sphere2.PrintState();
+            sphere3.PrintState();
+        }
+    }
+}
